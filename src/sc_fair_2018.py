427241a9edc2f5d8c196aa00cc7c0c9b37f4fd0c
Poorvi Sangli <poorvi@poorvis-air.attlocal.net>
2018-11-12 13:47:53 -0800

    initial set of files

---
+++ src/sc_fair_2018.py
@@ -0,0 +1,29 @@
+#Create a function that will:
+#Get the AM2302 readings
+#Send them to a newly-made file
+#Store them with the time
+
+import Adafruit_DHT
+import datetime
+import logging
+
+logger = logging.getLogger("sc_fair")
+
+def dht_readings():
+    "This function is opening a new file and sending the readings from the AM2302 to the file."
+    f = open("DHTreading_file", "w+")
+
+    while True:
+        current = datetime.datetime.now()
+        sensormodel = Adafruit_DHT.AM2302
+        sensorpin = 4
+        humidity, temperature = Adafruit_DHT.read_retry(sensormodel, sensorpin)
+        f.write(humidity, temperature, current)
+        logger.info("Collected sample at %s", str(current))
+        # TODO Sleep!!!
+
+    f.close()
+
+if __name__ == '__main__':
+    logger.info("Starting readings of humidity and temperature")
+    dht_readings()
